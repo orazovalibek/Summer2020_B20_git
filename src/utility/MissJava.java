701199a62a8ca27059450a00ce605e0bf36c1178
orazovalibek <68873962+orazovalibek@users.noreply.github.com>
2020-08-05 18:38:38 -0700

    Update MissJava.java

--- src/utility/MissJava.java
+++ src/utility/MissJava.java
@@ -3,6 +3,8 @@ package utility;
 public class MissJava {
     public static void main(String[] args) {
         
+        System.out.println("My name is Alibek");
+        
         System.out.println("You know what?");
 
         System.out.println("I love Java");
